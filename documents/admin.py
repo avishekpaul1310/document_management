f0ec726a8ed1560d36764594fd33994cdf1da4a1
avishekpaul1310 <avishekpaul1310@gmail.com>
2025-02-18 16:06:46 +0530

    commit

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Document
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'owner', 'uploaded_at', 'is_private')
+    list_filter = ('category', 'is_private', 'uploaded_at')
+    search_fields = ('title', 'description', 'owner__username')
+    date_hierarchy = 'uploaded_at'
