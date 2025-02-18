936eafcb55450483e4f6a1877f575cad4ed98009
avishekpaul1310 <avishekpaul1310@gmail.com>
2025-02-18 18:09:31 +0530

    commit

--- document_management/urls.py
+++ document_management/urls.py
@@ -4,12 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from documents import views as document_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('documents.urls')),
     # Authentication URLs
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html', next_page='login'), name='logout'),
     path('accounts/register/', document_views.register, name='register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
