8af34b80823e5b42948f4eb825523760a5a3712c
avishekpaul1310 <avishekpaul1310@gmail.com>
2025-02-18 16:56:27 +0530

    commit

--- documents/admin.py
+++ documents/admin.py
@@ -8,7 +8,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'owner', 'uploaded_at', 'is_private')
+    list_display = ('title', 'category', 'uploaded_by', 'uploaded_at', 'is_private')
     list_filter = ('category', 'is_private', 'uploaded_at')
-    search_fields = ('title', 'description', 'owner__username')
+    search_fields = ('title', 'description')
     date_hierarchy = 'uploaded_at'
+    readonly_fields = ('uploaded_by', 'uploaded_at')
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # Only set uploaded_by when creating new document
+            obj.uploaded_by = request.user
+        super().save_model(request, obj, form, change)
