c36bc8623d49921cf5a67237321b1192e477b1ab
avishekpaul1310 <avishekpaul1310@gmail.com>
2025-02-18 16:41:57 +0530

    commit

--- documents/templatetags/document_filters.py
+++ documents/templatetags/document_filters.py
@@ -3,5 +3,18 @@ from django import template
 register = template.Library()
 
 @register.filter
-def split(value, arg):
-    return value.split(arg)
+def split(value, key):
+    """
+    Returns the string split by key.
+    """
+    return value.split(key)
+
+@register.filter
+def get_file_extension(filename):
+    """
+    Returns the file extension from a filename.
+    """
+    try:
+        return filename.split('.')[-1].upper()
+    except (AttributeError, IndexError):
+        return ''
